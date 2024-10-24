d593ab3f0bf54bba5e4777f4ca2043b9f78fe077
Andrea Loyola <andrea.loyolad@ucuenca.edu.ec>
2024-10-24 16:33:11 -0500

    se mejoró home

---
+++ sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  let myCanvas = createCanvas(windowWidth, windowHeight);
+  myCanvas.parent("#my-p5-sketch"); //esto es un id por eso se utiliza #
+}
+function draw() {
+  background(0, 200, 180);
+}
